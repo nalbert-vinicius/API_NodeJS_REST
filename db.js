d94bedd1e39da88cc2dc2f002b0cb8b161caf38f
nalbert-vinicius <rullaxfox@gmail.com>
2021-06-01 20:12:33 -0300

    melhorias no insert do banco banco tbprodutos

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql');
-
 //criação da conexão com o Banco de dados
-var pool = mysql.createPool({
+var con = mysql.createConnection({
     //process.env.
     //são variaveis de ambiente configuradas no nodemon
-    "host" : process.env.MYSQL_HOST,
-    "port" : process.env.MYSQL_PORT,
     "user" : process.env.MYSQL_USER,
     "password" : process.env.MYSQL_PASSWORD,
     "database" : process.env.MYSQL_DATABASE,
-    insecureAuth : true
+    "host" : process.env.MYSQL_HOST,
+    "port" : process.env.MYSQL_PORT
 });
-
-module.exports = pool;
+con.connect((err) => {
+    if(!err){
+        console.log("Conectado com sucesso!");
+    }else{
+        console.log("A conexão falhou!")
+    }
+})
+exports.con = con;
--- rotas/produtos.js
+++ rotas/produtos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const route = express.Router();
-const mysql = require('../db.js');
+const mysql = require('../db.js').con;
+
 
 /**
  * req = request
@@ -31,24 +32,21 @@ route.post('/:parametro', (req, res, next) =>{
 })
 
 route.post('/', (req, res, next) => {
-    var nome = req.body.nome;
-    var preco = req.body.preco;
-        mysql.query('INSERT INTO produtos (nome, preco) VALUES ('+nome+','+preco+')',
-            //callback da query
-            (error, result, field) =>{
-                if(error){
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    });
-                }
-
-                res.status(201).send({
-                    msg: "Produto inserido com sucesso!",
-                    id_produto: result.insertId
-                })
+    mysql.query('INSERT INTO produtos (nome,preco) VALUES (?,?)',
+        [req.body.nome, req.body.preco],
+        (err, result, field) => {
+            if(err){
+               return res.status(500).send({
+                   msg: err,
+                   response: null
+               }) 
             }
-        ) 
+            res.status(201).send({
+                msg: "Produto inserido com sucesso!",
+                id: result.id
+            })
+        }
+    )
 })
 
 route.delete('/', (req, res, next) =>{
