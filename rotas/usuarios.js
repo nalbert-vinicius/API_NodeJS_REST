5051d1ef40062e745ecf78846468eae4cba28429
nalbert-vinicius <rullaxfox@gmail.com>
2021-06-05 14:03:06 -0300

    remoção bcrypt

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -2,7 +2,6 @@ const express = require('express');
 const route = express.Router();
 const mysql = require('../db').con;
 //biblioteca para criptografar senhas
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 route.post('/cadastro', (req, res, next) =>{
