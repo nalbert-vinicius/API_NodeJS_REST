9fa017dae1b7a64fee23d64e61d8aeced94f6f56
nalbert-vinicius <rullaxfox@gmail.com>
2021-06-05 22:06:48 -0300

    tratativa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const app = require('./app');
 //definindo a porta 
-const port = 3000;
+const port = process.env.PORT || 3000;
 //criação do servidor
 const server = http.createServer(app)
 //escutar a porta
